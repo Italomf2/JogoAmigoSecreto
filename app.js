b5f3c838718b56e665cea0182805e9275bd2ea51
Italomf2 <italomf44@gmail.com>
2025-03-06 15:21:34 -0300

    criar funcao para sortear amigos

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function limparCampo(){
 
 function adicionarAmigo() {
     const inputAmigo = document.getElementById('amigo');
-    
-    if (!inputAmigo.value || inputAmigo.value.trim().lenght === 0 || !isNaN(inputAmigo.value)){
+
+    if (!inputAmigo.value || inputAmigo.value.trim().length === 0 || !isNaN(inputAmigo.value)){
         alert("Por favor, insira um nome.")
 
     }else{
@@ -17,26 +17,46 @@ function adicionarAmigo() {
             alert('Esse amigo já foi adicionado')
         } else{
             amigos.push(inputAmigo.value)
+            atualizarLista();
+            limparCampo();
         }
-        limparCampo();
+  
     }
     
-    console.log(amigos)
 }
 
 function atualizarLista(){
     const lista = document.getElementById('listaAmigos');
     
-    lista.innerHTML = "";
+    lista.innerHTML = '';
 
-    for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement('li');
-        li.textContent = amigos[i];
-        lista.appendChild(li);
-    }
+    amigos.forEach((amigo, index) => {
+        const item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+      });
+
 }
 
+function sortearAmigo(){
 
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let nomeCorrespondenteAoIndice = amigos[indiceSorteado];
+    let listaSorteados = [];
+
+    if (amigos.length == 0){
+        alert('Não há ninguém para ser sorteado')
+    }
+
+    listaSorteados.push(nomeCorrespondenteAoIndice);
+    
+    const resultado = document.getElementById('resultado')
+    resultado.innerHTML = `O nome da pessoa sorteada é ${listaSorteados}`
+
+    atualizarLista();
+    limparCampo();
+
+}
 
 
 
